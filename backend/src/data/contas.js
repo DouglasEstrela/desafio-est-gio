0b14058dfcc7d19896bc63a76a41928ea5383372
Douglas Estrela <156541988+DouglasEstrela@users.noreply.github.com>
2026-06-30 16:14:56 -0300

    contas atualizadas

--- backend/src/data/contas.js
+++ backend/src/data/contas.js
@@ -1,8 +1,6 @@
 const contas = [
     {id: 1, tipo: "corrente", saldo: 1000},
-    {id: 2, tipo: "poupanca", saldo: 25},
-    {id: 3, tipo: "poupanca", saldo: 500},
-    {id: 4, tipo: "corrente", saldo: 10},
+    {id: 2, tipo: "poupanca", saldo: 25}
 ]
  
 module.exports = contas
